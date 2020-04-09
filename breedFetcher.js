b2cc851e6bb88fb244277ec7a2987472fc24d6ec
Sean Sullivan <dev@seanmedia.ca>
2020-04-09 19:53:36 +0000

    Add command line arguments

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,13 +2,17 @@
 
 const request = require('request');
 
-request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
+const breedName = process.argv[3];
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
   // console.log('error:', error);
   // console.log('statusCode:', response && response.statusCode);
   // console.log('body:', body);
   // console.log(typeof body);
 
   const data = JSON.parse(body);
-  console.log(data);
-  console.log(typeof data);
+  // console.log(data);
+  // console.log(typeof data);
+  const description = data[0].description;
+  console.log(description);
 });
