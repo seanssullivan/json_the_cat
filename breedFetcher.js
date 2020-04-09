50cd5797f680b34d30b9ed781cc6c55350465063
Sean Sullivan <dev@seanmedia.ca>
2020-04-09 20:07:54 +0000

    General refactoring and cleanup

--- breedFetcher.js
+++ breedFetcher.js
@@ -6,10 +6,7 @@ const breedName = process.argv[2];
 const query = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
 request(query, (error, response, body) => {
-  console.log('error:', error);
-  console.log('statusCode:', response && response.statusCode);
-  console.log('body:', body);
-  // console.log(typeof body);
+  
   if (error && error.code === 'ENOTFOUND') {
     console.log("Invalid url");
   } if (response !== 200) {
@@ -21,6 +18,5 @@ request(query, (error, response, body) => {
     const description = data[0]["description"];
     console.log(description);
   }
-  // console.log(data);
-  // console.log(typeof data);
+
 });
