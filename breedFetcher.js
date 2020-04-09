408674216a09b5a1b35133fb5d278123c34a2630
Sean Sullivan <dev@seanmedia.ca>
2020-04-09 19:50:32 +0000

    Parse JSON response

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,3 +1,14 @@
 // breedFetcher.js
 
 const request = require('request');
+
+request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
+  // console.log('error:', error);
+  // console.log('statusCode:', response && response.statusCode);
+  // console.log('body:', body);
+  // console.log(typeof body);
+
+  const data = JSON.parse(body);
+  console.log(data);
+  console.log(typeof data);
+});
